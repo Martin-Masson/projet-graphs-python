3bd5efbcf67aaedd92eb7bbeb71a64dde8443362
Martin-Masson <81696014+Martin-Masson@users.noreply.github.com>
2022-01-25 15:43:33 +0100

    Finished exercice 3
    Exo 4 and 5 still to be done. Also need to add documentation and test for all functions.

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -51,24 +51,23 @@ class node:
 	def add_parent_id(self, new_parent_id, multi=1):
         self.children[new_parent_id] = multi
 
-    def remove_parent_once(self, id):
-        if self.parents[id] < 2:
-             del self.parents[id]
+    def remove_parent_once(self, parent_id):
+        if self.parents[parent_id] < 2:
+            del self.parents[parent_id]
         else:
-            self.parents[id] -= 1
+            self.parents[parent_id] -= 1
 
-    def remove_child_once(self, id):
-        if self.children[id] < 2:
-             del self.children[id]
+    def remove_child_once(self, children_id):
+        if self.children[children_id] < 2:
+            del self.children[children_id]
         else:
-            self.children[id] -= 1
+            self.children[children_id] -= 1
     
-    def remove_parent_id(self, id):
-        del self.parents[id]
+    def remove_parent_id(self, parent_id):
+        del self.parents[parent_id]
     
-    def remove_child_id(self, id):
-        del self.children[id]
-
+    def remove_child_id(self, children_id):
+        del self.children[children_id]
 
 class open_digraph:  # for open directed graph
 	
@@ -141,7 +140,7 @@ class open_digraph:  # for open directed graph
 	def add_edge(self, src, tgt):
 		tgt_node = get_node_by_id(tgt)
 		src_node = get_node_by_id(src)
-		if tgt_node.parents.has_key(tsrc) && src_node.children.has_key(tgt):
+		if tgt_node.parents.has_key(src) and src_node.children.has_key(tgt):
 			tgt_node.parents[src] += 1
 			src_node.children[tgt] += 1
 		else:
@@ -156,5 +155,103 @@ class open_digraph:  # for open directed graph
 			self.add_edge(parent, node_id)
 			self.add_edge(node_id, children)
 
+    '''
     def remove_edge(self, src, tgt):
-        
+        tgt_node = get_node_by_id(tgt)
+		src_node = get_node_by_id(src)
+		if tgt_node.parents[src] > 1 and src_node.children.[tgt] > 1:
+			tgt_node.parents[src] -= 1
+			src_node.children[tgt] -= 1
+		else:
+			del tgt_node.parents[src]
+			del src_node.children[tgt]
+    '''
+
+    # *args is a tuple of tuples (src, tgt)
+    def remove_edges(self, *args):
+        for arg in args:
+            tgt = arg[0]
+            src = arg[1]
+            tgt_node = get_node_by_id(tgt)
+		    src_node = get_node_by_id(src)
+            if tgt_node.parents[src] > 1 and src_node.children.[tgt] > 1:
+			    tgt_node.parents[src] -= 1
+			    src_node.children[tgt] -= 1
+		    else:
+			    del tgt_node.parents[src]
+			    del src_node.children[tgt]
+    
+    '''
+    def remove_parallel_edge(self, src, tgt):
+        src_node = get_node_by_id(src)
+        tgt_node = get_node_by_id(tgt)
+        del src_node.children[tgt]
+		del tgt_node.parents[src]
+    '''
+
+    def remove_parallel_edges(self, *args):
+        for arg in args:
+            tgt = arg[0]
+            src = arg[1]
+            src_node = get_node_by_id(src)
+            tgt_node = get_node_by_id(tgt)
+            del src_node.children[tgt]
+		    del tgt_node.parents[src]
+
+    '''
+    def remove_node_by_id(self, tgt_id):
+        tgt_node = get_node_by_id(tgt_id)
+        tgt_node.set_parent_ids({})
+        tgt_node.set_children_ids({})
+    '''
+
+    def removes_node_by_id(self, *args):
+        for arg in args:
+            tgt_id = arg[0]
+            tgt_node = get_node_by_id(tgt_id)
+
+            for parent in tgt.node.parents:
+                parent.remove_child_id(tgt_id)
+
+            for child in tgt.children:
+                child.remove_parent_id(tgt_id)
+
+            tgt_node.set_parent_ids({})
+            tgt_node.set_children_ids({})
+    
+    def is_well_formed(self):
+        # Checking if all intput nodes are in the graph, have no parents and have a single child of multiplicity 1
+        for in_node in inputs:
+            if not(self.nodes.has_key(in_node)):
+                raise Exception(f"Input node {in_node} isn't in the graph")
+            if in_node.parents:
+                raise Exception(f"Input node {in_node} has parents")
+            if in_node.children.values() != [1]:
+                raise Exception(f"Intput node {in_node} has more than 1 children or its multiplicity is greater than 1")
+
+        # Checking if all output nodes are in the graph, have no children and have a single parent of multiplicity 1
+        for out_node in outputs:
+            if not(self.nodes.has_key(out_node)):
+                raise Exception(f"Output node {in_node} isn't in the graph")
+            if out_node.children:
+                raise Exception(f"Output node {in_node} has children")
+            if out_node.parents.values() != [1]:
+                raise Exception(f"Output node {in_node} has more than 1 parent or its multiplicity is greater than 1")
+            
+        # Checking if all the keys in nodes are paired with a node of id equal to the key
+        if self.nodes.keys() != node_id for node_id in self.nodes.values():
+            raise Exception(f"A key of <id,node> in nodes is invalid")
+
+        for node in self.nodes:
+
+            # Checking if all the childrens of all the nodes have that same node as a parent with their respective multiplicity
+            for parent_id, multi in node.parents.items():
+                childrens = get_node_by_id(parent_id).children
+                if not(node.id in childrens) or not(multi == childrens[node.id]):
+                    raise Exception(f"node {parent_id} isn't a parent of node {node_id} or their multiplicity are different")
+
+            # Checking if all the parents of all the nodes have that same node as a child with their respective multiplicity
+            for child_id, multi in node.children.items():
+                parents = get_node_by_id(parent_id).parents
+                if not(node.id in parents) or not(multi == parents[node.id]):
+                    raise Exception(f"node {child_id} isn't a child of node {node_id} or their multiplicity are different")
