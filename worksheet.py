d8beb1e835c815a9496da0256a2cf87bf0664d50
Martin-Masson <81696014+Martin-Masson@users.noreply.github.com>
2022-03-15 17:01:08 +0100

    Update worksheet.py

--- worksheet.py
+++ worksheet.py
@@ -1,8 +1,24 @@
 import inspect
+from tabnanny import verbose
 from modules.open_digraph import *
 
-n0 = Node(0, 'a', {}, {2:1})
-n1 = Node(1, 'b', {}, {2:1})
-n2 = Node(2, 'c', {0:1, 1:1}, {})
-od0 = OpenDigraph([0, 1], [2], [n0, n1, n2])
-print(od0)
+i0 = Node(0, "i0", {}, {3: 1})
+i1 = Node(1, "i1", {}, {3: 1})
+i2 = Node(2, "i2", {}, {4: 1})
+n0 = Node(3, "&", {0: 1, 1: 1}, {4: 1})
+n1 = Node(4, "|", {3: 1, 2: 1}, {5: 1})
+o0 = Node(5, "o0", {4: 1}, {})
+
+od1 = OpenDigraph([0, 1, 2], [5], [i0, i1, i2, n0, n1, o0])
+
+i3 = Node(0, "i3", {}, {1: 1})
+n0 = Node(1, "", {0: 1}, {2: 1})
+o2 = Node(2, "o2", {1: 1}, {})
+
+od2 = OpenDigraph([0], [2], [i3, n0, o2])
+
+od1.iparallel(od2)
+
+od1.display(verbose=True)
+
+print_matrix(od1.adjency_matrix())
